d9ec722feb29e8f742c31ae1d8fe5004322efd7a
Felix Buehler <account@buehler.rocks>
2017-07-08 01:33:29 +0200

    shell/aliases: removed old rsync from backup & added vnc-temp

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -84,7 +84,8 @@ alias makepasswd='makepasswd --minchars=10 --maxchars=25 --count=10'
 alias shuttle='sshuttle --dns -r uberspace 0/0'
 
 alias backupnow='sudo borgmatic -v 1 && echo $(date +"%Y%m%d") > ~/.borgbackup.log'
-alias syncbackupto='sudo rsync -aPzchv backup:/home/felix/data/ '
+
+alias vnc-temp='x11vnc -display :0 -nap -wait 50 -noxdamage -forever -rfbauth ~/.vnc/passwd'
 
 alias notify='notify-send Terminal "Process has been finished"'
 alias screen-off='xset -display :0.0 dpms force off'
