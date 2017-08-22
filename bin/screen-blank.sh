e822ffa142fa982e1453fef53c8b8e6cc70c55fe
Felix Buehler <account@buehler.rocks>
2017-08-22 17:33:04 +0200

    shell/aliases: removed old screen-blank-command

--- bin/screen-blank.sh
+++
@@ -1,6 +0,0 @@
-DUR="$(xset q | grep Standby | cut -d' ' -f4)"
-xset s off
-case $DUR in
-    7200) xset dpms 900 900 900;echo "Screen blank set to 15 mins.";;
-    *) xset dpms 7200 7200 7200;echo "Screen blank set to 2 hours.";;
-esac
