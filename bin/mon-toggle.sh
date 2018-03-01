bea5fdb8a50bb017083027481f8eeaeaf7dc8561
Felix Buehler <account@buehler.rocks>
2018-03-01 11:39:45 +0100

    [bin/mon-toggle] fixing xrandr not using sleep

--- bin/mon-toggle.sh
+++ bin/mon-toggle.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-xrandr 1> /dev/null
-sleep 2
-xrandr --output VGA1 --auto --pos 1600x0 --output LVDS1 --auto --pos 0x150 --primary --output HDMI1 --auto --output DP1 --auto
+#!/bin/bash
+intern=LVDS1
+extern=VGA1
+
+if xrandr | grep "$extern disconnected"; then
+    xrandr --output "$extern" --off --output "$intern" --auto
+else
+    xrandr --output "$intern" --auto --pos 0x150 --output "$extern" --auto --pos 1600x0 --primary --output HDMI1 --auto --output DP1 --auto
+fi
