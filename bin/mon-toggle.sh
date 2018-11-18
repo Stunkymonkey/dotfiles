e4c2ef9ef91f07bf0a01cbd0d208af79ac39526a
Felix Buehler <account@buehler.rocks>
2018-11-18 16:09:33 +0100

    [mon] make external screen the primary screen

--- bin/mon-toggle.sh
+++ bin/mon-toggle.sh
@@ -5,5 +5,5 @@ extern=VGA1
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto --primary
 else
-    xrandr --output "$intern" --auto --pos 0x150 --primary --output "$extern" --auto --pos 1600x0 --output HDMI1 --auto --output DP1 --auto
+    xrandr --output "$intern" --auto --pos 0x150 --output "$extern" --primary --auto --pos 1600x0 --output HDMI1 --auto --output DP1 --auto
 fi
