126a960395a0bc2f17f3fa8ee21956693ea4251c
Felix Buehler <account@buehler.rocks>
2019-01-27 18:17:04 +0100

    [i3/mon-toggle] update for new monitor

--- bin/mon-toggle.sh
+++ bin/mon-toggle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 intern=LVDS1
-extern=VGA1
+extern=HDMI3
 
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto --primary
 else
-    xrandr --output "$intern" --auto --pos 0x150 --output "$extern" --primary --auto --pos 1600x0 --output HDMI1 --auto --output DP1 --auto
+    xrandr --output "$intern" --auto --pos 0x180 --output "$extern" --primary --auto --pos 1600x0 --output VGA1 --auto --output HDMI1 --auto --output DP1 --auto
 fi
