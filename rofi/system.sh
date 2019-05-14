61dcf26a2c7350cf9f87e064973f8b41782b7cea
Felix Buehler <account@buehler.rocks>
2019-05-14 23:58:28 +0200

    [rofi & sway] initial config & adjusted sway config

---
+++ rofi/system.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot\nSuspend\nHibernate\nHybrid-sleep\nSuspend-then-hibernate" | rofi -dmenu -i)
+
+if [[ $chosen = "Logout" ]]; then
+	swaymsg exit
+	loginctl terminate-session "$(loginctl session-status | awk 'NR==1{print $1}')"
+elif [[ $chosen = "Shutdown" ]]; then
+	systemctl poweroff
+elif [[ $chosen = "Reboot" ]]; then
+	systemctl reboot
+elif [[ $chosen = "Suspend" ]]; then
+	systemctl suspend
+elif [[ $chosen = "Hibernate" ]]; then
+	systemctl hibernate
+elif [[ $chosen = "Hybrid-sleep" ]]; then
+	systemctl hibernate
+elif [[ $chosen = "Suspend-then-hibernate" ]]; then
+	systemctl suspend-then-hibernate
+fi
