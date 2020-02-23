3dd0d5ca25d5a256300d649119561754a53c51b2
Felix Buehler <account@buehler.rocks>
2020-02-23 17:34:12 +0100

    [i3] remove old config files

--- bin/mon-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-intern=LVDS1
-extern=HDMI3
-
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto --primary
-else
-    xrandr --output "$intern" --auto --pos 0x180 --output "$extern" --primary --auto --pos 1600x0 --output VGA1 --auto --output HDMI1 --auto --output DP1 --auto
-fi
