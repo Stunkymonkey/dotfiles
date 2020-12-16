129859254381b6ee2be06d96770fcbba8641fd10
Felix Buehler <account@buehler.rocks>
2020-12-16 18:08:29 +0100

    [bin/clean] add nix

--- bin/clean.sh
+++ bin/clean.sh
@@ -3,19 +3,23 @@
 case "$1" in
 	all)
 		$0 docker
+		$0 nix
 		$0 pacman
 		$0 yay
 		;;
 	docker)
 		docker system prune --force
 		;;
+  nix)
+    nix-collect-garbage --delete-old
+		;;
 	pacman)
 		pacman -Sc --noconfirm
 		;;
 	yay)
-	    yay -Sacc --noconfirm
-        ;;
+	  yay -Sacc --noconfirm
+    ;;
 	*)
-		echo "Please give parameter of: [all docker pacman yay]"
+		echo "Please give parameter of: [all docker nix pacman yay]"
 		;;
 esac
