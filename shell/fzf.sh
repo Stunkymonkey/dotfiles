52db2fbf6194b73fe19dc17a55547cc17b2fce6d
Felix Buehler <account@buehler.rocks>
2020-12-26 00:21:38 +0100

    [shell/fzf] also source if on nixos

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -2,3 +2,8 @@
 
 [ -r /usr/share/fzf/key-bindings.bash ] && . /usr/share/fzf/key-bindings.bash
 [ -r /usr/share/fzf/completion.bash ] && . /usr/share/fzf/completion.bash
+
+if command -v fzf-share >/dev/null; then
+  source "$(fzf-share)/key-bindings.bash"
+  source "$(fzf-share)/completion.bash"
+fi
