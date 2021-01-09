0dec178ad8d23a551f23c09a5d598d014398e71e
Felix Buehler <account@buehler.rocks>
2021-01-09 12:38:57 +0100

    [shell/aliases] fix sublime and start fwupdmgr with sudo

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,7 +3,7 @@
 #alias definitions
 alias cd..="cd .."
 alias gits="git status"
-alias subl='subl3'
+alias subl='sublime3'
 
 alias les="less"
 
@@ -46,7 +46,7 @@ io(){
 
 if [ ! -z "$(type sudo 2>/dev/null)" -a "$USER" != 'root' ]; then
 	#create sudo aliases WITHOUT leading s
-	for sudo in mount umount iftop iotop fsadm lvchange lvconvert lvcreate lvdisplay lvextend lvm lvmchange lvmconf lvmconfig lvmdiskscan lvmdump lvmetad lvmsadc lvmsar lvreduce lvremove lvrename lvresize lvs lvscan pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan vgcfgbackup vgcfgrestore vgchange vgck vgconvert vgcreate vgdisplay vgexport vgextend vgimport vgimportclone vgmerge vgmknodes vgreduce vgremove vgrename vgs vgscan vgsplit;
+	for sudo in fwupdmgr mount umount iftop iotop fsadm lvchange lvconvert lvcreate lvdisplay lvextend lvm lvmchange lvmconf lvmconfig lvmdiskscan lvmdump lvmetad lvmsadc lvmsar lvreduce lvremove lvrename lvresize lvs lvscan pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan vgcfgbackup vgcfgrestore vgchange vgck vgconvert vgcreate vgdisplay vgexport vgextend vgimport vgimportclone vgmerge vgmknodes vgreduce vgremove vgrename vgs vgscan vgsplit;
 	do
 		type $sudo > /dev/null 2>&1 && alias $sudo="sudo $sudo";
 	done
