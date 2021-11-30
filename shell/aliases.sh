dd69b0f91f380ab19e3c3e2c46c4df7e6c6e1904
Felix Buehler <account@buehler.rocks>
2021-11-30 23:24:14 +0100

    shell/aliases: replace youtube-dl with yt-dlp

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -15,9 +15,9 @@ alias :qw="exit"
 alias vim="vim -p"
 alias vi="vim -p"
 
-alias youtube-dl='youtube-dl -o "%(title)s.%(ext)s"'
-alias youtube-audio='youtube-dl -o "%(title)s.%(ext)s" --extract-audio'
-alias youtube-mp3='youtube-dl -o "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
+alias yt-dl='yt-dlp -o "%(title)s.%(ext)s"'
+alias yt-audio='yt-dlp -o "%(title)s.%(ext)s" --extract-audio'
+alias yt-mp3='yt-dlp -o "%(title)s.%(ext)s" --extract-audio --audio-format mp3'
 alias music-gain="find ~/Music -iname '*.mp3' -execdir mp3gain -q -k -p {} \+"
 alias music-fix="find ~/Music -print0 | xargs -P 0 -0 nice -n 19 mp3val -f"
 
