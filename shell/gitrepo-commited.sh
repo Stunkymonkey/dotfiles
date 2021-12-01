ab1d79433af66ae04d3b9c4a2dc5101843dff386
Felix Buehler <account@buehler.rocks>
2021-12-01 14:00:26 +0100

    shell/{gitrepo,nixos}-commited: hide in nix-shell

--- shell/gitrepo-commited.sh
+++ shell/gitrepo-commited.sh
@@ -4,16 +4,18 @@ case "$-" in
 	*i*)
 		# This shell is interactive, if not ignore this part
 		# If printing this in non-interactive shells, it'll may prevent scp to work
-		cfgrepo=`readlink ~/.profile.d/10-gitrepo-commited.sh`
-		repo=`dirname $cfgrepo`
-		changes=`git -C $repo status --porcelain | wc -l`
+		if [ -z "${IN_NIX_SHELL}" ]; then
+			cfgrepo=$(readlink ~/.profile.d/10-gitrepo-commited.sh)
+			repo=$(dirname "$cfgrepo")
+			changes=$(git -C "$repo" status --porcelain | wc -l)
 
-		if [ $changes -gt 0 ]; then
-			echo -e '\e[01;31m'                            >&2
-			echo -e '####################################' >&2
-			echo -e '# GIT-config-Repo is not commited! #' >&2
-			echo -e '####################################' >&2
-			echo -e '\e[0m'                                >&2
+			if [ "$changes" -gt 0 ]; then
+				printf '\e[01;31m\n'                            >&2
+				printf '####################################\n' >&2
+				printf '# GIT-config-Repo is not commited! #\n' >&2
+				printf '####################################\n' >&2
+				printf '\e[0m\n'                                >&2
+			fi
 		fi
 		;;
 esac
--- shell/nixos-commited.sh
+++ shell/nixos-commited.sh
@@ -4,15 +4,16 @@ case "$-" in
 	*i*)
 		# This shell is interactive, if not ignore this part
 		# If printing this in non-interactive shells, it'll may prevent scp to work
-		if [ -d "/etc/nixos" ]; then
-			changes=`git -C /etc/nixos status --porcelain | wc -l`
-
-			if [ $changes -gt 0 ]; then
-				echo -e '\e[01;31m'                            >&2
-				echo -e '######################################' >&2
-				echo -e '# NixOS-config-Repo is not commited! #' >&2
-				echo -e '######################################' >&2
-				echo -e '\e[0m'                                >&2
+		if [ -z "${IN_NIX_SHELL}" ]; then
+			if [ -d "/etc/nixos" ]; then
+				changes=$(git -C /etc/nixos status --porcelain | wc -l)
+				if [ "$changes" -gt 0 ]; then
+					printf '\e[01;31m\n'                              >&2
+					printf '######################################\n' >&2
+					printf '# NixOS-config-Repo is not commited! #\n' >&2
+					printf '######################################\n' >&2
+					printf '\e[0m\n'                                  >&2
+				fi
 			fi
 		fi
 		;;
