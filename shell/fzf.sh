b33e57d67a9cad7f5901bc2224582d9b4b8961d1
Felix Buehler <account@buehler.rocks>
2021-12-06 12:41:54 +0100

    shell/fzf: add multiple methods for loading

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 
-[ -r /usr/share/fzf/key-bindings.bash ] && . /usr/share/fzf/key-bindings.bash
-[ -r /usr/share/fzf/completion.bash ] && . /usr/share/fzf/completion.bash
+# Arch
+if [ "${BASH_VERSION}" ] && [ -r /usr/share/fzf/key-bindings.bash ]; then
+  # shellcheck source=/dev/null
+  . /usr/share/fzf/key-bindings.bash
+fi
+if [ "${BASH_VERSION}" ] && [ -r /usr/share/fzf/completion.bash ]; then
+  # shellcheck source=/dev/null
+  . /usr/share/fzf/completion.bash
+fi
 
-if command -v fzf-share >/dev/null; then
-  source "$(fzf-share)/key-bindings.bash"
-  source "$(fzf-share)/completion.bash"
+if [ "${ZSH_VERSION}" ] && [ -r /usr/share/fzf/key-bindings.zsh ]; then
+  # shellcheck source=/dev/null
+  . /usr/share/fzf/key-bindings.zsh
+fi
+if [ "${ZSH_VERSION}" ] && [ -r /usr/share/fzf/completion.zsh ]; then
+  # shellcheck source=/dev/null
+  . /usr/share/fzf/completion.zsh
+fi
+
+# NixOS
+if [ "${BASH_VERSION}" ] && command -v fzf-share >/dev/null; then
+  # shellcheck source=/dev/null
+  . "$(fzf-share)/key-bindings.bash"
+  # shellcheck source=/dev/null
+  . "$(fzf-share)/completion.bash"
+fi
+
+if [ "${ZSH_VERSION}" ] && command -v fzf-share >/dev/null; then
+  # shellcheck source=/dev/null
+  . "$(fzf-share)/key-bindings.zsh"
+  # shellcheck source=/dev/null
+  . "$(fzf-share)/completion.zsh"
 fi
