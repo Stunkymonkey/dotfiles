52bfbe6d9d8b7e65915eebc31855b7e6727f2e8b
Felix Buehler <account@buehler.rocks>
2025-11-26 21:30:11 +0100

    shell/keychain: remove deprecated 'agent-parameter

--- shell/keychain.sh
+++ shell/keychain.sh
@@ -2,6 +2,6 @@
 
 # Assert an ssh-agent (which is empty at the beginning)
 if command -v keychain > /dev/null 2>&1; then
-	eval "$(keychain -q --nogui --agents ssh --timeout 600 --eval)"
+	eval "$(keychain -q --nogui --timeout 600 --eval)"
 fi
 
