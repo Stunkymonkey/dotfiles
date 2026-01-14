1c9d2e7eba850ddc0df4a0b8e203312d7f1f88c5
Felix Buehler <account@buehler.rocks>
2026-01-14 21:19:18 +0100

    shell/fzf: update shell completion sourcing

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -1,35 +1,14 @@
 #!/usr/bin/env bash
 
-# Arch
-if [ "${BASH_VERSION}" ] && [ -r /usr/share/fzf/key-bindings.bash ]; then
-  # shellcheck source=/dev/null
-  . /usr/share/fzf/key-bindings.bash
-fi
-if [ "${BASH_VERSION}" ] && [ -r /usr/share/fzf/completion.bash ]; then
-  # shellcheck source=/dev/null
-  . /usr/share/fzf/completion.bash
-fi
-
-if [ "${ZSH_VERSION}" ] && [ -r /usr/share/fzf/key-bindings.zsh ]; then
-  # shellcheck source=/dev/null
-  . /usr/share/fzf/key-bindings.zsh
-fi
-if [ "${ZSH_VERSION}" ] && [ -r /usr/share/fzf/completion.zsh ]; then
-  # shellcheck source=/dev/null
-  . /usr/share/fzf/completion.zsh
-fi
-
-# NixOS
-if [ "${BASH_VERSION}" ] && command -v fzf-share >/dev/null; then
-  # shellcheck source=/dev/null
-  . "$(fzf-share)/key-bindings.bash"
-  # shellcheck source=/dev/null
-  . "$(fzf-share)/completion.bash"
-fi
-
-if [ "${ZSH_VERSION}" ] && command -v fzf-share >/dev/null; then
-  # shellcheck source=/dev/null
-  . "$(fzf-share)/key-bindings.zsh"
-  # shellcheck source=/dev/null
-  . "$(fzf-share)/completion.zsh"
+if command -v fzf >/dev/null 2>&1; then
+  if [ -n "${BASH_VERSION-}" ]; then
+    # shellcheck source=/dev/null
+    eval "$(fzf --bash)"
+  elif [ -n "${ZSH_VERSION-}" ]; then
+    # shellcheck source=/dev/null
+    source <(fzf --zsh)
+  elif [ -n "${FISH_VERSION-}" ]; then
+    # shellcheck source=/dev/null
+    fzf --fish | source
+  fi
 fi
