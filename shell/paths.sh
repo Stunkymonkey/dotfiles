b526c1cc90f26246d719131e8435fc1236ed64ac
Felix Buehler <account@buehler.rocks>
2026-01-27 00:08:54 +0100

    shell/paths: remove perl

--- shell/paths.sh
+++ shell/paths.sh
@@ -15,17 +15,6 @@ appendpath () {
 appendpath ~/.local/bin
 appendpath ~/.bin
 
-if which ruby >/dev/null 2>&1 && which gem >/dev/null 2>&1; then
-    appendpath "$(ruby -r rubygems -e 'puts Gem.user_dir')"/bin
-fi
-
-# PERL cpan modules
-appendpath "$HOME"/.perl/5/bin
-export PERL5LIB="$HOME/.perl/5/lib/perl5:$PERL5LIB"
-export PERL_LOCAL_LIB_ROOT="$HOME/.perl/5:$PERL_LOCAL_LIB_ROOT"
-export PERL_MB_OPT="--install_base \"$HOME/.perl/5\""
-export PERL_MM_OPT="INSTALL_BASE=$HOME/.perl/5"
-
 export GOPATH=$HOME/.go
 export GOBIN=$HOME/.gobin
 appendpath "$GOPATH"/bin
